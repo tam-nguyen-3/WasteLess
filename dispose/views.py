eeccc52a3f6030783626adba5fcf8c4238688bfb
Tam Nguyen <tamngminh3@gmail.com>
2024-04-07 03:40:52 -0700

    revert

--- dispose/views.py
+++ dispose/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template("dispose/index.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
 
